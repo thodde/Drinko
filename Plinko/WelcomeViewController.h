8ce9e4f0f10cbef859c8ff49597ca6228e8af957
Trevor Hodde <trhodde@wpi.edu>
2014-02-16 18:38:53 -0500

    Adding welcome menu

---
+++ Plinko/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  Plinko
+//
+//  Created by Trevor Hodde on 2/16/14.
+//  Copyright (c) 2014 Josh Rooke-Ley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WelcomeViewController : NSObject
+
+@end
---
+++ Plinko/WelcomeViewController.m
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.m
+//  Plinko
+//
+//  Created by Trevor Hodde on 2/16/14.
+//  Copyright (c) 2014 Josh Rooke-Ley. All rights reserved.
+//
+
+#import "WelcomeViewController.h"
+
+@implementation WelcomeViewController
+
+//[self performSegueWithIdentifier: @"StartGameSeque" sender: self];
+
+@end
