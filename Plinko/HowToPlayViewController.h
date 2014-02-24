cb98dfba38dfc653e7f9becebe185e19d6bbd3be
Trevor Hodde <trhodde@wpi.edu>
2014-02-24 18:54:02 -0500

    All view controllers exist, just have to make them pretty

---
+++ Plinko/HowToPlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  HowToPlayViewController.h
+//  Plinko
+//
+//  Created by Trevor Hodde on 2/24/14.
+//  Copyright (c) 2014 Josh Rooke-Ley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HowToPlayViewController : UINavigationController
+
+@end
---
+++ Plinko/HowToPlayViewController.m
@@ -0,0 +1,42 @@
+//
+//  HowToPlayViewController.m
+//  Drinko
+//
+//  Created by Trevor Hodde on 2/24/14.
+//  Copyright (c) 2014 Trevor Hodde. All rights reserved.
+//
+
+#import "HowToPlayViewController.h"
+
+@interface HowToPlayViewController ()
+
+@end
+
+@implementation HowToPlayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) performSegueWithIdentifier {
+    //[super performSegueWithIdentifier];
+}
+
+@end
